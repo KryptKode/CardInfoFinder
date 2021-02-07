4d6a97a5e7ebec60e6627cc0a265585b00fea05a
kryptkode <paulakhamiogu@gmail.com>
2021-02-07 19:12:22 +0100

    fix: try ocr with google text Api

--- app/src/main/java/com/kryptkode/cardinfofinder/ui/ocrinput/OcrInputFragment.kt
+++ app/src/main/java/com/kryptkode/cardinfofinder/ui/ocrinput/OcrInputFragment.kt
@@ -1,12 +1,83 @@
 package com.kryptkode.cardinfofinder.ui.ocrinput
 
+import android.os.Bundle
+import android.view.View
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.kryptkode.cardinfofinder.R
 import com.kryptkode.cardinfofinder.databinding.FragmentOcrInputBinding
+import com.kryptkode.cardinfofinder.util.ocr.TextReaderAnalyzer
 import com.kryptkode.cardinfofinder.util.viewbinding.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import java.util.concurrent.Executors
 
+@AndroidEntryPoint
 class OcrInputFragment : Fragment(R.layout.fragment_ocr_input) {
 
     private val binding by viewBinding(FragmentOcrInputBinding::bind)
+
+    private val cameraExecutor by lazy { Executors.newSingleThreadExecutor() }
+
+    private val imageAnalyzer by lazy {
+        ImageAnalysis.Builder()
+            .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+            .build()
+            .also {
+                it.setAnalyzer(
+                    cameraExecutor,
+                    TextReaderAnalyzer(::onTextFound)
+                )
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(
+            {
+                val preview = Preview.Builder()
+                    .build()
+                    .also { it.setSurfaceProvider(binding.cameraPreviewView.surfaceProvider) }
+                cameraProviderFuture.get().bind(preview, imageAnalyzer)
+            },
+            ContextCompat.getMainExecutor(requireContext())
+        )
+    }
+
+    private fun ProcessCameraProvider.bind(
+        preview: Preview,
+        imageAnalyzer: ImageAnalysis
+    ) = try {
+        unbindAll()
+        bindToLifecycle(
+            viewLifecycleOwner,
+            CameraSelector.DEFAULT_BACK_CAMERA,
+            preview,
+            imageAnalyzer
+        )
+    } catch (ise: IllegalStateException) {
+        // Thrown if binding is not done from the main thread
+        Timber.e(ise, "Binding failed")
+    }
+
+
+    private fun onTextFound(foundText: String) {
+        Timber.d("We got new text: $foundText")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        cameraExecutor.shutdown()
+    }
 }
 
